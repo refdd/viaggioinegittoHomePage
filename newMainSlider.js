ba5e403dbb4b1f513380949d00f0f43728eeeb9d
refdd <titorefat76@gmail.com>
2023-05-12 02:31:10 +0200

    first commit

---
+++ newMainSlider.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Splide("#blogContainer", {
+        type   : 'loop',
+        perPage: 3, // Number of visible slides
+        gap: "30px", // Gap between slides
+        focus: 'center',
+    
+        pagination: true,
+        breakpoints: {
+        1024: {
+          perPage: 3,
+        },
+        767: {
+          perPage: 2,
+        },
+        640: {
+          perPage: 1,
+        },
+        autoScroll: {
+          speed: 1,
+        },
+  
+        // autoScroll: {
+        //   speed: 1,
+        // },
+      },
+      classes: {
+		pagination: 'splide__pagination your-class-pagination',
+		page      : 'splide__pagination__page your-class-page',
+       },
+    }).mount();
+  });
